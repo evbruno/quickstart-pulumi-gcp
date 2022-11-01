12f897fb09dd3b5580b955239d74899795ac0b91
Eduardo V. Bruno <eduardo.bruno@salesfloor.net>
2022-11-01 11:22:10 -0300

    add a few resources

---
+++ README.md
@@ -0,0 +1,6 @@
+# (not so) Quick Start with Pulumi and GCP
+
+command history:
+
+- `gcloud auth login`
+- `pulumi up`
--- index.ts
+++ index.ts
@@ -1,18 +1,41 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
+const name = 'quickstart';
 
-// class QuickStartResources extends pulumi.ComponentResource {
-// }
-
-// Create a GCP resource (Storage Bucket)
-const bucket = new gcp.storage.Bucket("my-bucket-v2", {
+const bucket = new gcp.storage.Bucket(`bucket-${name}`, {
     location: "US"
 });
 
-const topic = new gcp.pubsub.Topic("my-topic", {}, {});
+const topic = new gcp.pubsub.Topic(`topic-${name}`, {}, {});
+
+const sa = new gcp.serviceaccount.Account(
+  "my-service-account",
+  {
+    accountId: pulumi.interpolate`sa-${name}`,
+  }
+);
+
+new gcp.storage.BucketIAMMember(
+  `${name}-bucket-iam`,
+  {
+    bucket: bucket.id,
+    member: pulumi.interpolate`serviceAccount:${sa.email}`,
+    role: 'roles/storage.objectAdmin',
+  }
+);
+
+new gcp.pubsub.TopicIAMMember(
+  `${name}-topic-iam`,
+  {
+    topic: topic.id,
+    member: pulumi.interpolate`serviceAccount:${sa.email}`,
+    role: 'roles/pubsub.publisher',
+  }
+);
 
 // Export the DNS name of the bucket
 export const bucketName = bucket.url;
-
 export const topicId = topic.id;
+export const topicName = topic.name;
+export const saEmail = sa.email;
